3319e75892861b92e3f754b91746d1e4633f6352
inno <innocentiahan14@gmail.com>
2026-04-29 09:45:18 +0800

    feat: init springboot

---
+++ ucm-discuss-be/src/test/java/com/example/ucm_discuss_be/UcmDiscussBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ucm_discuss_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcmDiscussBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
