8b2710ec2407282f80105149e85154bdea14248c
inno <innocentiahan14@gmail.com>
2026-05-19 01:39:53 +0800

    chore: remove pendiunguploadcontext

--- ucm-discuss-fe/ucmdiscuss/app/(tabs)/(home)/_layout.tsx
+++ ucm-discuss-fe/ucmdiscuss/app/(tabs)/(home)/_layout.tsx
@@ -3,38 +3,41 @@ import { useTheme } from '@/context/ThemeContext';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { HomeHeader } from '@/components/header/homeHeader';
 import { SearchProvider, useSearch } from "@/context/SearchContext";
+import { PendingUploadsProvider } from '@/context/PendingUploadsContext';
 
 export default function HomeStackLayout() {
   const { theme } = useTheme();
 
   return (
-    <SearchProvider>
-      <Stack
-        screenOptions={{
-          headerStyle: { 
-              backgroundColor: theme.colors.background 
-          },
-          headerShadowVisible: false,
-        }}
-      >
-        <Stack.Screen 
-          name="index" 
-          options={{ 
-              header: () => (
-                <SafeAreaView
-                  edges={['top']}
-                  style={{ backgroundColor: theme.colors.primary }}>
-                  <LayoutHeaderWrapper />
-                </SafeAreaView>
-              )
-          }} 
-        />
-        <Stack.Screen 
-          name="[id]" 
-          options={{ title: 'Thread Detail' }} 
-        />
-      </Stack>
-    </SearchProvider>
+    // <PendingUploadsProvider>
+      <SearchProvider>
+        <Stack
+          screenOptions={{
+            headerStyle: { 
+                backgroundColor: theme.colors.background 
+            },
+            headerShadowVisible: false,
+          }}
+        >
+          <Stack.Screen 
+            name="index" 
+            options={{ 
+                header: () => (
+                  <SafeAreaView
+                    edges={['top']}
+                    style={{ backgroundColor: theme.colors.primary }}>
+                    <LayoutHeaderWrapper />
+                  </SafeAreaView>
+                )
+            }} 
+          />
+          <Stack.Screen 
+            name="[id]" 
+            options={{ title: 'Thread Detail' }} 
+          />
+        </Stack>
+      </SearchProvider>
+    // </PendingUploadsProvider>
   );
 }
 
