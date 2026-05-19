bab43e3fd39ee22ed901188e0af0f83fce88a35f
Kmuzaki <keihanmuzaki0480@gmail.com>
2026-05-19 12:09:45 +0800

    chore: revise routes

--- ucm-discuss-be/src/main/java/com/example/ucm_discuss_be/threadAttachments/ThreadAttachmentController.java
+++ ucm-discuss-be/src/main/java/com/example/ucm_discuss_be/threadAttachments/ThreadAttachmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/thread-attachments")
+@RequestMapping("/api/threads/attachments")
 public class ThreadAttachmentController {
 
     @Autowired
