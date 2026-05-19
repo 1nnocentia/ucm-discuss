1fa451eab94674d6c824f3c3b4827c0499de2139
inno <innocentiahan14@gmail.com>
2026-05-20 02:22:05 +0800

    chore: update button

--- ucm-discuss-fe/ucmdiscuss/components/buttons/commentButton.tsx
+++ ucm-discuss-fe/ucmdiscuss/components/buttons/commentButton.tsx
@@ -1,36 +1,36 @@
 import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@/context/ThemeContext';
-import { Post } from '@/models/user';
 import { router } from 'expo-router';
 
 interface CommentButtonProps {
-    post: Post;
+    count: number;
+    targetId: string;
     onPress?: () => void;
 }
 
-export default function CommentButton({ post, onPress }: CommentButtonProps) {
+export default function CommentButton({ count, targetId, onPress }: CommentButtonProps) {
     const { theme } = useTheme();
-    const commentCount = post.comments;
+
     const handleCommentAction = () => {
         if (onPress) {
             onPress();
         } else {
             router.push({
                 pathname: '/threads/[id]',
-                params: { id: post.id, focusInput: 'true' }
+                params: { id: targetId, focusInput: 'true' }
             });
         }
-    }
+    };
 
     return (
         <TouchableOpacity style={styles.commentGroup} onPress={handleCommentAction} activeOpacity={0.7}>
             <Ionicons name="chatbubble-outline" size={18} color={theme.colors.textSecondary} />
-            <Text style={[styles.footerText, { color: theme.colors.textSecondary, fontFamily: theme.fonts.openSans }]}>
-                {commentCount}
+            <Text style={[styles.footerText, { color: theme.colors.textSecondary, fontFamily: theme.fonts.openSans }]}> 
+                {count}
             </Text>
         </TouchableOpacity>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
